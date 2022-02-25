44d0d0012cf4f81ea9bfff6298a755ac647d25ed
root <root@ocp-bastion.dataserv.local>
2022-02-25 18:29:51 +0300

    fixed

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "flag"
+    //"flag"
     "github.com/altugyildirim/kubernetesinventory/actions"
+    //"./actions"
 )
 
 func main() {
 
 
-    cmd := flag.String("cmd", "list", "Should be a string")
-    ns := flag.String("ns", "default", "Namespace is a string")
-    resource := flag.String("rs", "pods", "Resource is a string")
-    file := flag.String("file", "pods", "Resource is a string")
+    //cmd := flag.String("cmd", "list", "Should be a string")
+    //ns := flag.String("ns", "default", "Namespace is a string")
+    //resource := flag.String("rs", "pods", "Resource is a string")
+    //file := flag.String("file", "pods", "Resource is a string")
 
 
-    flag.Parse()
-
+    //flag.Parse()
     actions.List("deploy","default")
-//    switch *cmd {
-//    case "deploy":
-//        actions.Deploy(file)
-//    default:
-//        actions.List(resource, ns)
-//    }
+    //switch *cmd {
+    //case "deploy":
+    //    actions.Deploy(file)
+    //default:
+    //    actions.List(resource, ns)
+    //}
 }
 
